841b354e74ca58323184e62ff89984070e3144e4
Akshay Dhiman <akshay.virgo@gmail.com>
2019-01-09 18:46:39 +0530

    index content updates

---
+++ src/assets/js/main.js
@@ -0,0 +1,14 @@
+window.onclick = function(e) {
+  if (e.target.parentElement.className === 'index-faq__item--toggle') {
+    var parent = e.target.parentElement;
+    var text = parent.parentElement.getElementsByClassName('index-faq__item--answer')[0];
+    var img = parent.firstChild;
+    if (text.style.display === 'none') {
+      text.style.display = 'block';
+      img.src = 'assets/img/arrow-up.svg';
+    } else {
+      text.style.display = 'none';
+      img.src = 'assets/img/arrow-down.svg';
+    }
+  }
+}
